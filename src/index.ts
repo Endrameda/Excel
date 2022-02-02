0f11808218f8b5f159dfb71a0ccc9e5c295f071e
Sardor <Endrameda98@gmail.com>
2022-02-03 00:59:08 +0500

    add styles and html templates

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
-import './module';
-import './scss/index.scss';
+import './scss/index.scss'
 
-console.log('Works!');
-
-const start = async () => Promise.resolve((callback: any) => callback('Promis is work!!!'));
-
-start().then(console.log);
-
-const returnString = (str: string) => str;
-
-returnString('tralala');
+console.log('Working!')
