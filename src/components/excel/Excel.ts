e03aaf443abacbc897bfe42d315ae3730253f8b2
Sardor <Endrameda98@gmail.com>
2022-02-04 11:50:54 +0500

    fix ts errors

--- src/components/excel/Excel.ts
+++ src/components/excel/Excel.ts
@@ -1,6 +1,9 @@
 import { ExcelOptionsType, ExcelSelectorType } from './types';
 
 export class Excel {
+  private $el: Element | null;
+  private components: Array<any>;
+
   constructor(selector: ExcelSelectorType, options: ExcelOptionsType) {
     this.$el = document.querySelector(selector);
     this.components = options.components || [];
