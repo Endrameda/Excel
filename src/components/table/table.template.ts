2a52add21d7e5a26d138da453bb716099bc3760d
Sardor <Endrameda98@gmail.com>
2022-02-13 14:55:59 +0500

    Table.template

--- src/components/table/table.template.ts
+++ src/components/table/table.template.ts
@@ -1,3 +1,38 @@
-export function createTable() {
-  return '<h1>Table</h1>';
-}
+const CODES = {
+  A: 65,
+  Z: 90,
+};
+
+const createCol = (): string => `
+    <div class="column">
+    A
+    </div>
+  `;
+
+const createRow = (content?: string): string => `
+    <div class="row">
+        <div class="row-info"></div>
+        <div class="row-data">${content}</div>
+    </div>
+  `;
+
+const createCell = (): string => `
+  <div class="cell" contenteditable="">A1</div>
+`;
+
+export const createTable = (rowsCount: number = 15): string => {
+  const colsCount = CODES.Z - CODES.A;
+  const rows = [];
+
+  const cols = new Array(colsCount).fill('');
+
+  console.log(cols);
+
+  rows.push(createRow());
+
+  for (let i = 0; i < rowsCount; i += 1) {
+    rows.push(createRow());
+  }
+
+  return rows.join('');
+};
