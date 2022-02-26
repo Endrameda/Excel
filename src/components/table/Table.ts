ad6cb273b312bb5053c4c1dcdd59e7022988d6fa
Sardor <Endrameda98@gmail.com>
2022-02-26 16:33:48 +0500

    table resizer finished

--- src/components/table/Table.ts
+++ src/components/table/Table.ts
@@ -1,7 +1,7 @@
 import { ExcelComponent } from '../../core/ExcelComponent';
 import { createTable } from './table.template';
-import { $, DOM } from '../../core/DOM/DOM';
-import { resizersType } from './types';
+import { DOM } from '../../core/DOM/DOM';
+import { shouldResize, TableResizeHandler } from './helpers';
 
 export class Table extends ExcelComponent {
     static className = 'excel__table';
@@ -18,55 +18,6 @@ export class Table extends ExcelComponent {
     }
 
     onMousedown(event: MouseEvent) {
-        const element = event.target as HTMLElement;
-
-        if (element.dataset?.resize) {
-            const $resizer = $(event.target as HTMLElement);
-            const $parent = $resizer.closest('[data-type="resizable"]');
-            const cords = $parent.getClientCords();
-            const type = $resizer.data.resize;
-            const sideProp = type === resizersType.col ? 'bottom' : 'right';
-
-            let value: number;
-
-            const cols = type === resizersType.col
-                ? this.$root.findAll(`[data-col="${$parent.data.col}"]`)
-                : [];
-            const rows = type === resizersType.row
-                ? this.$root.findAll(`[data-row="${$parent.data.row}"]`)
-                : [];
-
-            $resizer.css({ opacity: 1, [sideProp]: '-9999px' });
-
-            document.onmousemove = (mousemoveEvent) => {
-                if (type === resizersType.col) {
-                    const delta = mousemoveEvent.pageX - cords!.right;
-                    value = cords!.width + delta;
-                    $resizer.css({ right: `${-delta}px` });
-                } else {
-                    const delta = mousemoveEvent.pageY - cords!.bottom;
-                    value = cords!.height + delta;
-
-                    $resizer.css({ bottom: `${-delta}px` });
-                }
-            };
-
-            document.onmouseup = () => {
-                document.onmousemove = null;
-                document.onmouseup = null;
-
-                if (type === resizersType.col) {
-                    cols.forEach((el: HTMLElement) => {
-                        el.style.width = `${value}px`;
-                    });
-                } else {
-                    rows.forEach((el: HTMLElement) => {
-                        el.style.height = `${value}px`;
-                    });
-                }
-
-                $resizer.css({ opacity: 0, bottom: 0, right: 0 });
-            };
-        }
+        TableResizeHandler(event, this.$root);
     }
 }
--- src/components/table/helpers.ts
+++ src/components/table/helpers.ts
@@ -1,5 +1,7 @@
 import { CODES } from './constants';
 import { Nullable } from '../../types/globalTypes';
+import { $ } from '../../core/DOM/DOM';
+import { resizersType } from './types';
 
 export const createRow = (index: Nullable<number>, content?: string): string => {
     const resizer = index ? '<div class="row-resize" data-resize="row"></div>' : '';
@@ -27,3 +29,59 @@ export const toCell = (_: string, col: number): string => `
     `;
 
 export const toChar = (_: string, index: number) => String.fromCharCode(CODES.A + index);
+
+export const TableResizeHandler = (
+    event: MouseEvent,
+    $root: { findAll: (arg1: string) => any; },
+) => {
+    const element = event.target as HTMLElement;
+
+    if (element.dataset?.resize) {
+        const $resizer = $(event.target as HTMLElement);
+        const $parent = $resizer.closest('[data-type="resizable"]');
+        const cords = $parent.getClientCords();
+        const type = $resizer.data.resize;
+        const sideProp = type === resizersType.col ? 'bottom' : 'right';
+
+        let value: number;
+
+        const cols = type === resizersType.col
+            ? $root.findAll(`[data-col="${$parent.data.col}"]`)
+            : [];
+        const rows = type === resizersType.row
+            ? $root.findAll(`[data-row="${$parent.data.row}"]`)
+            : [];
+
+        $resizer.css({ opacity: 1, [sideProp]: '-9999px' });
+
+        document.onmousemove = (mousemoveEvent) => {
+            if (type === resizersType.col) {
+                const delta = mousemoveEvent.pageX - cords!.right;
+                value = cords!.width + delta;
+                $resizer.css({ right: `${-delta}px` });
+            } else {
+                const delta = mousemoveEvent.pageY - cords!.bottom;
+                value = cords!.height + delta;
+
+                $resizer.css({ bottom: `${-delta}px` });
+            }
+        };
+
+        document.onmouseup = () => {
+            document.onmousemove = null;
+            document.onmouseup = null;
+
+            if (type === resizersType.col) {
+                cols.forEach((el: HTMLElement) => {
+                    el.style.width = `${value}px`;
+                });
+            } else {
+                rows.forEach((el: HTMLElement) => {
+                    el.style.height = `${value}px`;
+                });
+            }
+
+            $resizer.css({ opacity: 0, bottom: 0, right: 0 });
+        };
+    }
+};
